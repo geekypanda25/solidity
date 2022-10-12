72a9cb5a44371ce6bbebca99aafd90eb2b4f5d43
shlok-dt6 <saishlok@hotmail.com>
2022-10-11 21:51:11 -0700

    updates

---
+++ tests/GovTest.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+import "../contracts/GovFactory.sol"
+
+contract tester {
+
+}
+
+0x80C8659a2F335DDfC707bE12F795D1f8d952C262
